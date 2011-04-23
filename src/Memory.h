d2964df25d881d811b825bed0e48db4a4b5856a8
Tony Levi <tlevi@internode.on.net>
2011-04-23 14:54:55 +0930

    Memory: enable alignment checks, add unaligned operations, refactoring

--- src/Memory.h
+++ src/Memory.h
@@ -9,11 +9,40 @@
 class Memory{
 public:
 
-	template <class T> T get(uInt addr);
+	template <class T> T const getu(const uInt addr);
+	template <class T> T* const getpu(const uInt addr);
 
-	template <class T> void set(uInt addr, T val);
 
-	template <class T> T* getp(uInt addr);
+	template <class T> void const checkAlign(const uInt addr) const{
+		// this should really throw an exception
+		// in the vm for now we explode
+		if (addr % sizeof(T) != 0)
+			fatalError("Misaligned access\n");
+	};
+
+
+	template <class T> T const get(const uInt addr){
+		checkAlign<T>(addr);
+		return getu<T>(addr);
+	};
+
+
+	template <class T> T* const getp(const uInt addr){
+		checkAlign<T>(addr);
+		return getpu<T>(addr);
+	};
+
+
+	template <class T> void const set(const uInt addr, const T val){
+		checkAlign<T>(addr);
+		*getpu<T>(addr) = val;
+	};
+
+
+	template <class T> void const setu(const uInt addr, const T val){
+		*getpu<T>(addr) = val;
+	};
+
 
 	Memory();
 	~Memory();
@@ -27,15 +56,7 @@ private:
 };
 
 
-template <class T>
-T Memory::get(uInt addr){
-#if DEBUGLEVEL > 1
-	// this should really throw an exception in the vm
-	// for now we explode when some debugging is on
-	if (addr % sizeof(T) != 0)
-		fatalError("Misaligned access\n");
-#endif
-
+template <class T> T const Memory::getu(const uInt addr){
 	const uInt page_addr = addr & PAGE_MASK;
 	const uInt page_bits = addr & PAGE_BITS;
 
@@ -46,13 +67,7 @@ T Memory::get(uInt addr){
 };
 
 
-template <class T>
-T* Memory::getp(uInt addr){
-#if DEBUGLEVEL > 1
-	if (addr % sizeof(T) != 0)
-		fatalError("Misaligned access\n");
-#endif
-
+template <class T> T* const Memory::getpu(const uInt addr){
 	const uInt page_addr = addr & PAGE_MASK;
 	const uInt page_bits = addr & PAGE_BITS;
 
@@ -64,12 +79,6 @@ T* Memory::getp(uInt addr){
 	page = create_page(page_addr);
 	storage.setpage(page_addr, page);
 	return static_cast<T*>(page) + page_bits;
-};
-
-
-template <class T>
-void Memory::set(uInt addr, T val){
-	*getp<T>(addr) = val;
 };
 
 
